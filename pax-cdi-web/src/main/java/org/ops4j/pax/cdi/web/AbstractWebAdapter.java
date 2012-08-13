d5093aa08a9b09623a0afbe6d7780b17a9a2534e
Harald Wellmann <harald.wellmann@gmx.de>
2012-08-13 19:41:14 +0200

    added license headers, applied default formatting

--- pax-cdi-web/src/main/java/org/ops4j/pax/cdi/web/AbstractWebAdapter.java
+++ pax-cdi-web/src/main/java/org/ops4j/pax/cdi/web/AbstractWebAdapter.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.cdi.web;
 
 import javax.servlet.ServletContextListener;
@@ -14,55 +31,57 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 public abstract class AbstractWebAdapter extends AbstractLifecycle {
 
-	protected BundleContext bundleContext;
-	private ServiceTracker<CdiContainer, CdiContainer> cdiContainerTracker;
-	private CdiWebAppDependencyManager dependencyManager;
-	private ReplaceableService<HttpService> httpService;
+    protected BundleContext bundleContext;
+    private ServiceTracker<CdiContainer, CdiContainer> cdiContainerTracker;
+    private CdiWebAppDependencyManager dependencyManager;
+    private ReplaceableService<HttpService> httpService;
 
-	protected AbstractWebAdapter(BundleContext bundleContext) {
-		this.bundleContext = bundleContext;
-	}
+    protected AbstractWebAdapter(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
 
-	@Override
-	protected void onStart() {
-		cdiContainerTracker = new ServiceTracker<CdiContainer, CdiContainer>(bundleContext,
-			CdiContainer.class, new CdiContainerListener());
-		cdiContainerTracker.open();
+    @Override
+    protected void onStart() {
+        cdiContainerTracker = new ServiceTracker<CdiContainer, CdiContainer>(bundleContext,
+            CdiContainer.class, new CdiContainerListener());
+        cdiContainerTracker.open();
 
-		dependencyManager = new CdiWebAppDependencyManager(bundleContext, getServletContextListener());
+        dependencyManager = new CdiWebAppDependencyManager(bundleContext,
+            getServletContextListener());
 
-		httpService = new ReplaceableService<HttpService>(bundleContext, HttpService.class, dependencyManager);
-		httpService.start();
-	}
+        httpService = new ReplaceableService<HttpService>(bundleContext, HttpService.class,
+            dependencyManager);
+        httpService.start();
+    }
 
-	protected abstract ServletContextListener getServletContextListener();
+    protected abstract ServletContextListener getServletContextListener();
 
-	@Override
-	protected void onStop() {
-		httpService.stop();
-		cdiContainerTracker.close();
-	}
+    @Override
+    protected void onStop() {
+        httpService.stop();
+        cdiContainerTracker.close();
+    }
 
-	private class CdiContainerListener implements
-		ServiceTrackerCustomizer<CdiContainer, CdiContainer> {
+    private class CdiContainerListener implements
+        ServiceTrackerCustomizer<CdiContainer, CdiContainer> {
 
-		@Override
-		public CdiContainer addingService(ServiceReference<CdiContainer> reference) {
-			CdiContainer cdiContainer = bundleContext.getService(reference);
-			dependencyManager.addCdiContainer(cdiContainer);
-			return cdiContainer;
-		}
+        @Override
+        public CdiContainer addingService(ServiceReference<CdiContainer> reference) {
+            CdiContainer cdiContainer = bundleContext.getService(reference);
+            dependencyManager.addCdiContainer(cdiContainer);
+            return cdiContainer;
+        }
 
-		@Override
-		public void modifiedService(ServiceReference<CdiContainer> reference, CdiContainer service) {
-			dependencyManager.removeCdiContainer(service);
-			CdiContainer cdiContainer = bundleContext.getService(reference);
-			dependencyManager.addCdiContainer(cdiContainer);
-		}
+        @Override
+        public void modifiedService(ServiceReference<CdiContainer> reference, CdiContainer service) {
+            dependencyManager.removeCdiContainer(service);
+            CdiContainer cdiContainer = bundleContext.getService(reference);
+            dependencyManager.addCdiContainer(cdiContainer);
+        }
 
-		@Override
-		public void removedService(ServiceReference<CdiContainer> reference, CdiContainer service) {
-			dependencyManager.removeCdiContainer(service);
-		}
-	}
+        @Override
+        public void removedService(ServiceReference<CdiContainer> reference, CdiContainer service) {
+            dependencyManager.removeCdiContainer(service);
+        }
+    }
 }
--- pax-cdi-web/src/main/java/org/ops4j/pax/cdi/web/impl/CdiWebAppDependencyManager.java
+++ pax-cdi-web/src/main/java/org/ops4j/pax/cdi/web/impl/CdiWebAppDependencyManager.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.cdi.web.impl;
 
 import java.util.Dictionary;
@@ -21,99 +38,98 @@ import org.slf4j.LoggerFactory;
 
 public class CdiWebAppDependencyManager implements ReplaceableServiceListener<HttpService> {
 
-	private static Logger logger = LoggerFactory.getLogger(CdiWebAppDependencyManager.class);
+    private static Logger logger = LoggerFactory.getLogger(CdiWebAppDependencyManager.class);
 
-	private BundleContext bundleContext;
-	private Map<Long, ServiceRegistration<WebAppDependencyHolder>> registrations = 
-		new HashMap<Long, ServiceRegistration<WebAppDependencyHolder>>();
-	private Map<Long, CdiContainer> webApps = new HashMap<Long, CdiContainer>();
-	private HttpService httpService;
-	private ServletContextListener servletContextListener;
+    private BundleContext bundleContext;
+    private Map<Long, ServiceRegistration<WebAppDependencyHolder>> registrations = new HashMap<Long, ServiceRegistration<WebAppDependencyHolder>>();
+    private Map<Long, CdiContainer> webApps = new HashMap<Long, CdiContainer>();
+    private HttpService httpService;
+    private ServletContextListener servletContextListener;
 
-	public CdiWebAppDependencyManager(BundleContext bundleContext, ServletContextListener listener) {
-		this.bundleContext = bundleContext;
-		this.servletContextListener = listener;
-	}
+    public CdiWebAppDependencyManager(BundleContext bundleContext, ServletContextListener listener) {
+        this.bundleContext = bundleContext;
+        this.servletContextListener = listener;
+    }
 
-	@Override
-	public synchronized void serviceChanged(HttpService oldService, HttpService newService) {
-		for (ServiceRegistration<WebAppDependencyHolder> registration : registrations.values()) {
-			registration.unregister();
-		}
-		httpService = newService;
-		for (long bundleId : webApps.keySet()) {
-			register(bundleId, webApps.get(bundleId));
-		}
-	}
+    @Override
+    public synchronized void serviceChanged(HttpService oldService, HttpService newService) {
+        for (ServiceRegistration<WebAppDependencyHolder> registration : registrations.values()) {
+            registration.unregister();
+        }
+        httpService = newService;
+        for (long bundleId : webApps.keySet()) {
+            register(bundleId, webApps.get(bundleId));
+        }
+    }
 
-	private void register(long bundleId, CdiContainer cdiContainer) {
-		if (httpService != null) {
+    private void register(long bundleId, CdiContainer cdiContainer) {
+        if (httpService != null) {
 
-			HttpService webAppHttpService = getProxiedHttpService(bundleId);
-			CdiServletContainerInitializer initializer = new CdiServletContainerInitializer(
-				cdiContainer, servletContextListener);
-			WebAppDependencyHolder dependencyHolder = new CdiWebAppDependencyHolder(
-				webAppHttpService, initializer);
-			Dictionary<String, String> props = new Hashtable<String, String>();
-			props.put("bundle.id", Long.toString(bundleId));
-			ServiceRegistration<WebAppDependencyHolder> registration = bundleContext
-				.registerService(WebAppDependencyHolder.class, dependencyHolder, props);
-			registrations.put(bundleId, registration);
-			logger.info("registered WebAppDependencyHolder for bundle [{}]",
-				cdiContainer.getBundle());
-		}
-	}
+            HttpService webAppHttpService = getProxiedHttpService(bundleId);
+            CdiServletContainerInitializer initializer = new CdiServletContainerInitializer(
+                cdiContainer, servletContextListener);
+            WebAppDependencyHolder dependencyHolder = new CdiWebAppDependencyHolder(
+                webAppHttpService, initializer);
+            Dictionary<String, String> props = new Hashtable<String, String>();
+            props.put("bundle.id", Long.toString(bundleId));
+            ServiceRegistration<WebAppDependencyHolder> registration = bundleContext
+                .registerService(WebAppDependencyHolder.class, dependencyHolder, props);
+            registrations.put(bundleId, registration);
+            logger.info("registered WebAppDependencyHolder for bundle [{}]",
+                cdiContainer.getBundle());
+        }
+    }
 
-	/**
-	 * The HTTP Service is proxied per web app (TODO why?) - see {@link HttpServiceFactory} and its
-	 * use in the pax-web-runtime Activator. Since the proxied service also wraps the referencing
-	 * bundle, we make sure to obtain the correct proxy via the bundle context of the extended web
-	 * bundle instead of using our own {@code httpService} member which is registered to the
-	 * extender bundle.
-	 * 
-	 * @param bundleId
-	 *            bundle ID of extended web bundle
-	 * @return
-	 */
-	private HttpService getProxiedHttpService(long bundleId) {
-		Bundle webAppBundle = bundleContext.getBundle(bundleId);
-		BundleContext webAppBundleContext = webAppBundle.getBundleContext();
-		ServiceReference<HttpService> httpServiceRef = webAppBundleContext
-			.getServiceReference(HttpService.class);
-		HttpService webAppHttpService = webAppBundleContext.getService(httpServiceRef);
-		return webAppHttpService;
-	}
+    /**
+     * The HTTP Service is proxied per web app (TODO why?) - see {@link HttpServiceFactory} and its
+     * use in the pax-web-runtime Activator. Since the proxied service also wraps the referencing
+     * bundle, we make sure to obtain the correct proxy via the bundle context of the extended web
+     * bundle instead of using our own {@code httpService} member which is registered to the
+     * extender bundle.
+     * 
+     * @param bundleId
+     *            bundle ID of extended web bundle
+     * @return
+     */
+    private HttpService getProxiedHttpService(long bundleId) {
+        Bundle webAppBundle = bundleContext.getBundle(bundleId);
+        BundleContext webAppBundleContext = webAppBundle.getBundleContext();
+        ServiceReference<HttpService> httpServiceRef = webAppBundleContext
+            .getServiceReference(HttpService.class);
+        HttpService webAppHttpService = webAppBundleContext.getService(httpServiceRef);
+        return webAppHttpService;
+    }
 
-	private void unregister(long bundleId) {
-		ServiceRegistration<WebAppDependencyHolder> registration = registrations.get(bundleId);
-		if (registration != null) {
-			try {
-				registration.unregister();
-			}
-			catch (IllegalStateException exc) {
-				// ignore if already unregistered
-			}
-		}
-	}
+    private void unregister(long bundleId) {
+        ServiceRegistration<WebAppDependencyHolder> registration = registrations.get(bundleId);
+        if (registration != null) {
+            try {
+                registration.unregister();
+            }
+            catch (IllegalStateException exc) {
+                // ignore if already unregistered
+            }
+        }
+    }
 
-	public synchronized void addCdiContainer(CdiContainer webApp) {
-		Bundle bundle = webApp.getBundle();
-		if (isWebBundle(bundle)) {
-			long bundleId = bundle.getBundleId();
-			webApps.put(bundleId, webApp);
-			register(bundleId, webApp);
-		}
-	}
+    public synchronized void addCdiContainer(CdiContainer webApp) {
+        Bundle bundle = webApp.getBundle();
+        if (isWebBundle(bundle)) {
+            long bundleId = bundle.getBundleId();
+            webApps.put(bundleId, webApp);
+            register(bundleId, webApp);
+        }
+    }
 
-	private boolean isWebBundle(Bundle bundle) {
-		return bundle.getHeaders().get(WebContainerConstants.CONTEXT_PATH_KEY) != null;
-	}
+    private boolean isWebBundle(Bundle bundle) {
+        return bundle.getHeaders().get(WebContainerConstants.CONTEXT_PATH_KEY) != null;
+    }
 
-	public synchronized void removeCdiContainer(CdiContainer webApp) {
-		long bundleId = webApp.getBundle().getBundleId();
-		if (webApps.containsKey(bundleId)) {
-			unregister(bundleId);
-			webApps.remove(bundleId);
-		}
-	}
+    public synchronized void removeCdiContainer(CdiContainer webApp) {
+        long bundleId = webApp.getBundle().getBundleId();
+        if (webApps.containsKey(bundleId)) {
+            unregister(bundleId);
+            webApps.remove(bundleId);
+        }
+    }
 }
