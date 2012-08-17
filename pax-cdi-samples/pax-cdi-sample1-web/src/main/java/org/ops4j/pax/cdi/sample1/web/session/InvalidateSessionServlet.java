3f0e66dc1afe1cef4edebfe1120d3578a6be3917
Harald Wellmann <harald.wellmann@gmx.de>
2012-08-17 10:25:37 +0200

    [PAXCDI-13] test servlets and beans for session scope

---
+++ pax-cdi-samples/pax-cdi-sample1-web/src/main/java/org/ops4j/pax/cdi/sample1/web/session/InvalidateSessionServlet.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.cdi.sample1.web.session;
+
+import java.io.IOException;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/invalidate")
+public class InvalidateSessionServlet extends HttpServlet {
+
+	@Inject
+	private BeanManager beanManager;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/text");
+
+		if (request.getParameter("timeout") != null) {
+			SimpleSessionBean.setBeanDestroyed(false);
+			request.getSession().setMaxInactiveInterval(Integer.parseInt(request.getParameter("timeout")));
+		}
+		else if (request.getParameter("isBeanDestroyed") != null) {
+			response.getWriter().print(SimpleSessionBean.isBeanDestroyed());
+		}
+		else {
+			SimpleSessionBean.setBeanDestroyed(false);
+			request.getSession().invalidate();
+		}
+	}
+}
---
+++ pax-cdi-samples/pax-cdi-sample1-web/src/main/java/org/ops4j/pax/cdi/sample1/web/session/SimpleSessionBean.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.cdi.sample1.web.session;
+
+import java.io.Serializable;
+
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.SessionScoped;
+
+@SuppressWarnings("serial")
+@SessionScoped
+class SimpleSessionBean implements Serializable {
+
+	private static boolean beanDestroyed = false;
+	private final long timestamp = System.currentTimeMillis();
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	@PreDestroy
+	public void destroyBean() {
+		beanDestroyed = true;
+	}
+
+	public static boolean isBeanDestroyed() {
+		return beanDestroyed;
+	}
+
+	public static void setBeanDestroyed(boolean beanDestroyed) {
+		SimpleSessionBean.beanDestroyed = beanDestroyed;
+	}
+}
