e036377cb4de7919e3c3f734d4781692298ac300
Harald Wellmann <harald.wellmann@gmx.de>
2013-04-14 17:01:06 +0200

    [PAXCDI-59] integration test for component lifecycle

---
+++ pax-cdi-itest/src/test/java/org/ops4j/pax/cdi/test/ComponentLifecycleTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.cdi.test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.cdi.test.TestConfiguration.regressionDefaults;
+import static org.ops4j.pax.cdi.test.TestConfiguration.workspaceBundle;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.cdi.sample1.IceCreamService;
+import org.ops4j.pax.cdi.sample1.client.IceCreamClient;
+import org.ops4j.pax.cdi.sample1.client.StrawberryClient;
+import org.ops4j.pax.cdi.sample1.client.StrawberryService;
+import org.ops4j.pax.cdi.spi.CdiContainerFactory;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.util.Filter;
+import org.ops4j.pax.swissbox.tracker.ServiceLookup;
+import org.ops4j.pax.swissbox.tracker.ServiceLookupException;
+import org.osgi.framework.BundleContext;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class ComponentLifecycleTest {
+
+    @Inject
+    private CdiContainerFactory containerFactory;
+
+    @Inject
+    @Filter(timeout = 1000000)
+    private IceCreamClient client;
+    
+    @Inject
+    private BundleContext bc;
+
+    @Configuration
+    public Option[] config() {
+        return options(
+            regressionDefaults(),
+
+            workspaceBundle("pax-cdi-samples/pax-cdi-sample1"),
+            workspaceBundle("pax-cdi-samples/pax-cdi-sample1-client"),
+            workspaceBundle("pax-cdi-extender"),
+            workspaceBundle("pax-cdi-extension"),
+            workspaceBundle("pax-cdi-api"),
+            workspaceBundle("pax-cdi-spi"),
+            workspaceBundle("pax-cdi-openwebbeans"),
+
+            mavenBundle("org.apache.openwebbeans", "openwebbeans-impl").versionAsInProject(),
+            mavenBundle("org.apache.openwebbeans", "openwebbeans-spi").versionAsInProject(),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.javassist").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.bundles", "scannotation").versionAsInProject(),
+            mavenBundle("org.apache.xbean", "xbean-bundleutils").versionAsInProject(),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.asm").versionAsInProject(), //
+            mavenBundle("org.slf4j", "jul-to-slf4j").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-servlet_3.0_spec").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-validation_1.0_spec").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-jcdi_1.0_spec").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-interceptor_1.1_spec").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-el_2.2_spec").versionAsInProject());
+
+    }
+
+    @Test(expected = ServiceLookupException.class)
+    public void componentWithUnsatisfiedDependencyShouldNotBeRegistered(){
+        ServiceLookup.getService(bc, StrawberryClient.class, 10);
+    }
+    
+    @Test
+    public void componentShouldRegisterWhenDependencyAppears(){
+        StrawberryService strawberryService = new StrawberryService();
+        Dictionary<String,String> props = new Hashtable<String, String>();
+        props.put("flavour", "strawberry");
+        bc.registerService(IceCreamService.class, strawberryService, props);
+        StrawberryClient strawberryClient = ServiceLookup.getService(bc, StrawberryClient.class, 100);
+        assertThat(strawberryClient, is(notNullValue()));
+        assertThat(strawberryClient.getFlavour(), is("Strawberry"));
+    }
+}
