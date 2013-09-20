13836c2f2a8b08e11258c794cdc6aef4a4867f0d
Harald Wellmann <harald.wellmann@gmx.de>
2013-09-20 20:29:00 +0200

    [PAXCDI-67] Karaf feature pax-cdi-openwebbeans is incomplete
    fixed Karaf features and added integration tests

---
+++ itest/src/it/itest-karaf/src/test/java/org/ops4j/pax/cdi/test/karaf/PaxCdiWebOpenWebBeansTest.java
@@ -0,0 +1,41 @@
+package org.ops4j.pax.cdi.test.karaf;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.cdi.test.karaf.RegressionConfiguration.regressionDefaults;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.cdi.api.Info;
+import org.ops4j.pax.cdi.spi.CdiContainerFactory;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+
+@RunWith(PaxExam.class)
+public class PaxCdiWebOpenWebBeansTest {
+
+    @Inject
+    private CdiContainerFactory factory;
+
+    @Configuration
+    public Option[] config() {
+        return new Option[] { 
+            regressionDefaults(),
+            
+            features(
+                maven().groupId("org.ops4j.pax.cdi").artifactId("pax-cdi-features").type("xml")
+                    .classifier("features").version(Info.getPaxCdiVersion()), "pax-cdi-web-openwebbeans")            
+            };
+    }
+
+    @Test
+    public void test() throws Exception {
+        assertThat(factory, is(notNullValue()));
+    }
+}
---
+++ itest/src/it/itest-karaf/src/test/java/org/ops4j/pax/cdi/test/karaf/RegressionConfiguration.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2011 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.cdi.test.karaf;
+
+import static org.ops4j.pax.exam.CoreOptions.composite;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.propagateSystemProperty;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.when;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+
+import java.io.File;
+
+import org.ops4j.pax.exam.ConfigurationManager;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.karaf.options.configs.CustomProperties;
+import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+
+/**
+ * Default configuration for native container regression tests, overriding the default test system
+ * configuration.
+ * <p>
+ * We do not need the Remote Bundle Context for Native Container, and we prefer unified logging with
+ * logback.
+ * <p>
+ * To override the standard options, you need to set the configuration property
+ * {@code pax.exam.system = default}.
+ * 
+ * @author Harald Wellmann
+ * @since Dec 2011
+ */
+public class RegressionConfiguration {
+
+    public static Option regressionDefaults() {
+        return regressionDefaults(null);        
+    }
+
+    public static Option regressionDefaults(String unpackDir) {
+        return composite(
+
+            karafDistributionConfiguration().frameworkUrl(mvnKarafDist()).karafVersion(karafVersion()).
+                unpackDirectory(unpackDir == null ? null : new File(unpackDir)).useDeployFolder(false),                
+            
+                configureConsole().ignoreLocalConsole(),
+            
+            when(isEquinox()).useOptions(                
+                editConfigurationFilePut(CustomProperties.KARAF_FRAMEWORK, "equinox"),
+                propagateSystemProperty("pax.exam.framework"),
+                systemProperty("osgi.console").value("6666"),
+                systemProperty("osgi.console.enable.builtin").value("true")));        
+    }
+
+    public static boolean isEquinox() {
+        return "equinox".equals(System.getProperty("pax.exam.framework"));
+    }
+
+    public static boolean isFelix() {
+        return "felix".equals(System.getProperty("pax.exam.framework"));
+    }
+    
+    public static MavenArtifactUrlReference mvnKarafDist() {
+        return maven().groupId("org.apache.karaf")
+            .artifactId("apache-karaf").type("zip").version(karafVersion());
+    }
+    
+    public static String karafVersion() {
+        ConfigurationManager cm = new ConfigurationManager();
+        String karafVersion = cm.getProperty("pax.exam.karaf.version", "3.0.0.RC1");
+        return karafVersion;
+    }
+}
