245114a8d43187ad2b981d865ef8c914f08e33c5
martin.schaefer <martin.schaefer@orcasys.ch>
2015-03-01 22:01:26 +0100

    added mising files

---
+++ itest/src/it/itest-cdi10/src/test/java/org/ops4j/pax/cdi/test/RankingPropertyTest.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.cdi.test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.cdiProviderBundles;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.paxCdiProviderAdapter;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.regressionDefaults;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.workspaceBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.cdi.sample7.api.RankedService;
+import org.ops4j.pax.cdi.spi.CdiContainer;
+import org.ops4j.pax.cdi.spi.CdiContainerFactory;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class RankingPropertyTest {
+	
+	@Inject
+	private CdiContainerFactory	containerFactory;
+	
+	@Inject
+	private BundleContext		bc;
+	
+	@Configuration
+	public Option[] config() {
+		return options(
+				regressionDefaults(),
+				workspaceBundle("org.ops4j.pax.cdi.samples", "pax-cdi-sample7-api"),
+				workspaceBundle("org.ops4j.pax.cdi.samples", "pax-cdi-sample7-service-impl100"),
+				paxCdiProviderAdapter(),
+				cdiProviderBundles());
+	}
+	
+	@Test
+	public void checkContainerFactory() {
+		assertThat(this.containerFactory.getContainers().size(), is(1));
+		CdiContainer cdiContainer = this.containerFactory.getContainers().iterator().next();
+		assertThat(cdiContainer.getBundle().getSymbolicName(), is("org.ops4j.pax.cdi.sample7.service.impl100"));
+	}
+	
+	@Test
+	public void checkRankingProperty() {
+		ServiceReference<RankedService> rankedServiceReference = this.bc.getServiceReference(RankedService.class);
+		assertThat(rankedServiceReference, is(notNullValue()));
+		assertThat((Integer) rankedServiceReference.getProperty(Constants.SERVICE_RANKING), is(100));
+	}
+	
+}
