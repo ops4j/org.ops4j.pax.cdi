ada0aaa7ed4dd1f6158e1acb7a0c5d35ea3e65d9
puce77 <fbrunnerreg@gmx.ch>
2015-05-06 22:51:38 +0200

    PAXCDI-164 @OsgiServiceProvider: Support ParameterizedType - Sample and Unit Test

---
+++ itest/src/it/itest-standalone/src/test/java/org/ops4j/pax/cdi/test/ParameterizedTypeSupportTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.cdi.test;
+
+import javax.inject.Inject;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.cdi.api.OsgiService;
+import org.ops4j.pax.cdi.sample8.SampleServiceWithGenericTypeParameter;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.cdiProviderBundles;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.paxCdiProviderAdapter;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.regressionDefaults;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.workspaceBundle;
+import org.ops4j.pax.exam.Configuration;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class ParameterizedTypeSupportTest {
+
+
+    @Inject
+    @OsgiService
+    private SampleServiceWithGenericTypeParameter<Object> sampleService;
+
+    @Configuration
+    public Option[] config() {
+        return options(
+                regressionDefaults(),
+                workspaceBundle("org.ops4j.pax.cdi.samples", "pax-cdi-sample8"),
+                paxCdiProviderAdapter(),
+                cdiProviderBundles());
+    }
+
+    @Test
+    public void testParameterizedTypeSupported() {
+        assertNotNull(sampleService);
+        assertNotNull(sampleService.foo());
+    }
+
+}
